aff36a7aba2718d8f84a35ed2f10bed921490db5
yuegang0511 <yuegang.5@163.com>
2019-11-03 16:56:30 +0800

    clock and main sequence

--- MF_layer2_class.py
+++ MF_layer2_class.py
@@ -3,16 +3,20 @@ import random
 import time
 
 
+# 定义的数据以文件格式进行存储，文件名为子站的id，时长为1s，文件格式txt文件，内容为0，1比特，每行根据传输速率定义为一个时隙传输的二进制内容
 class Layer2(object):
-    # 数据链路层的原始数据函数，定义传输速率speed 默认128 传输时间 time 默认1s
+    # 数据链路层的原始数据函数，定义传输速率speed 单位为k/s，默认128 传输时间 time 默认1s，slot_num为一帧的时隙个数
     # 定义生成数据函数，输出为二进制比特流
     # 补充协议相关数据，突发类型，6种
     # 返回一个数据列表
-    def __init__(self,speed = 128, time = 1):
+    def __init__(self, speed=128, time=1, id="test", slot_num=64):
         self.speed = speed
         self.time = time
-        self.data = []
-        self.source_data()
+        self.data = ""
+        # self.source_data()
+        self.filename = str(id)
+        self.slot_num = slot_num
+        self.write_source_data()
 
     def source_data(self):
         # 计算需要产生多少个原始二进制数据
@@ -20,12 +24,25 @@ class Layer2(object):
         # 计算每个二进制数据时间间隔
         betimes = 1 / self.speed
         i = 0
-        while i < size :
+        self.data = ""
+        while i < size:
             bit_data = random.randint(0, 100) % 2
-            self.data.append(bit_data)
+            self.data = self.data + str(bit_data)
             i += 1
             # time.sleep(betimes)
 
+    def write_source_data(self):
+        i = 0
+        # if os.path.exists(self.filename):
+        file = open(self.filename, "w")
+        while i < self.slot_num:
+            self.source_data()
+            file.write(self.data)
+            file.write("\n")
+            i += 1
+        file.close()
+
 
 # layer2 = Layer2()
+# layer2.write_source_data()
 # print(layer2.data)
--- MF_substation_class.py
+++ MF_substation_class.py
@@ -1,23 +1,62 @@
 from MF_layer2_class import *
 from MF_module_class import *
 import numpy as np
+import random
 from matplotlib import pyplot as plt
 from scipy.fftpack import fft, ifft
 
 
+def random_generate(num):
+    random_data = ""
+    for i in range(num):
+        random_data = random_data + str(random.randint(0, 100) % 2)
+    return random_data
+
+
 class Substation(object):
     # 需要一个链路层数据源 layer_frame 字典形式（speed=128，time=1） 传递两个参数，一个为速率，一个为时长（时长的一个帧的时长）生成了一个帧长的数据
+    # 子站有一个唯一id，由mainstation分配
     # 需要一个调制方式
     # 需要频分复用过程中的载频和时隙信息，数据格式为字典格式，键为时隙信息，值为频率信息，以一个帧为例
     # 输出时域和频域的结果
-    def __init__(self, fsinfo, layer_frame):
+    def __init__(self, fsinfo, layer_frame, id="aaaaaaaa"):
         self.frequency = []
         self.slot = []
-        self.source_data = MF_layer2_class.Layer2(layer_frame["speed"],layer_frame["time"])
+        self.id = id
+        self.mac = random_generate(32)
+        # self.mac_generate()
+        # 定义csc信息变量，固定格式，长度欸112bit，包含子站的mac地址,定义前序为16bit，mac地址为32bit
+        self.csc_info = ""
+        self.pre_csc = random_generate(16)
+        # 定义acq信息变量,包长较短，载频的数据为16bit
+        self.acq = ""
+        self.pre_acp = random_generate(16)
+        # 定义sync信息变量，由前导和业务信令业务心灵为128bit
+        self.sync = ""
+        self.pre_sync = random_generate(16)
+        # 生成都对应id的数据文件
+        self.source_data = MF_layer2_class.Layer2(layer_frame["speed"], layer_frame["time"], self.id)
+        self.pre_data = random_generate(16)
         # 定义一个函数，对类中的变量进行赋值
         for key, value in fsinfo.items():
             self.frequency.append(value)
             self.slot.append(key)
+        # 初始化三类突发数据
+        self.acq_generate(500)
+        self.csc_generate()
+        self.sync_generate()
+
+    def csc_generate(self):
+        plugin_data = random_generate(64)
+        self.csc_info = self.pre_csc + self.mac + plugin_data
+
+    def acq_generate(self, frequency_num):
+        frequency = "%016d" % int(bin(frequency_num)[2:])
+        self.acq = self.pre_acp + frequency
+
+    def sync_generate(self):
+        plugin_data = random_generate(128)
+        self.sync = self.pre_sync + plugin_data
 
     def module_data(self):
         # 计算时隙数
@@ -25,7 +64,8 @@ class Substation(object):
         # 计算每个时隙的符号数
         slot_data_num = len(self.source_data.data) // slot_num
         # 取出每个时隙的数据
-        slot_datas = [self.source_data.data[i: i+slot_data_num] for i in range(0, len(self.source_data.data), slot_data_num)]
+        slot_datas = [self.source_data.data[i: i + slot_data_num] for i in
+                      range(0, len(self.source_data.data), slot_data_num)]
         # 对时隙中的数据按照调制方式以及频点进行跳频调制
         # 每个时隙传输的比特数是一样的
         for i in range(slot_num):
@@ -33,13 +73,18 @@ class Substation(object):
             slot_info = self.slot[i]
             frequency_info = self.frequency[i]
             # 进行数据调制
-            
+
         # print(slot_datas)
 
 
 dict1 = {"12": "1", "22": "3"}
 layer_frame = {"speed": 128, "time": 1}
 substation = Substation(dict1, layer_frame)
+
+print(substation.mac)
+print(substation.csc_info)
+print(substation.sync)
+print(substation.acq)
 # substation.module_data()
 # print(substation.source_data.data)
 # print(substation.frequency)
